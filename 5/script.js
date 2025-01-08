877b9f3c31506b3885e1b9081fcd406000971b3f
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-08 21:59:22 +0100

    a5 finished

--- 5/script.js
+++ 5/script.js
@@ -1,26 +1,46 @@
 // Base URL
-const apiUrl = "https://api.example.com/data";
+const apiUrl = `https://opentdb.com/api.php`;
 
 // Function to make a GET request using
 // TODO: 1. find the error and fix it, 2. finish the function
-function fetchData(url) {
+function fetchData(url=cat) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+      const results = data.results;
+      return results;
+    })
+
+
+    .catch(error => {
+  console.error("Somethign went wrong!" + error);
     });
-}
+} 
 
 fetchData(apiUrl);
 
 // Function to make a POST request
 // TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+}
+
 
 const dataToPost = {
   name: "John Doe",
