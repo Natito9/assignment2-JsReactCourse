c255b68de8854182b3af4bcf029707f31affc87c
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-15 22:37:49 +0100

    7

--- 7/script.js
+++ 7/script.js
@@ -7,7 +7,17 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(string) {
+  let reverse = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+      reverse += string[i];
+  }
+  return reverse;
+}
+console.log(reverseString("hello"))
+console.log(reverseString("world"))
+console.log(reverseString("12345"))
+
 
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
@@ -18,8 +28,17 @@ function reverseString() {}
  *   reverseNumber(101010) should return 10101
  */
 
-function reverseNumber() {}
-
+function reverseNumber(n) {
+  let number = n.toString()
+  let reverse = "";
+  for (let i = number.length - 1; i >= 0; i--) {
+      reverse += number[i];
+  }
+  return reverse;
+}
+console.log(reverseNumber(12345))
+console.log(reverseNumber(98765))
+console.log(reverseNumber(101010))
 /**
  * TODO destruct the following object to get name and age.
  */
@@ -31,12 +50,20 @@ const person = {
     job: "Developer",
   },
 };
+const {name, info: {age, city, job}} = person;
+console.log(name, age);
+
+
 
 /**
  * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
  */
 
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
+const [first, second, ...rest] = thisArray
+console.log(first) //apple
+console.log(second) //banana
+console.log(rest) //['cherry', 'dates', 'elderberry', 'fig']
 
 /**
  * !!!!OPTIONAL!!!!
@@ -50,4 +77,24 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("coding") should return false
  */
 
-function isPalindrome() {}
+function isPalindrome(word) {
+  let reverse = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+      reverse += word[i];
+  }
+  
+  if (reverse === word) {
+    console.log("true")
+    
+  }
+  else {
+    console.log("false")
+  }
+}
+
+   isPalindrome("madam") 
+   isPalindrome("hello") 
+   isPalindrome("12321") 
+   isPalindrome("racecar") 
+   isPalindrome("coding") 
+//end
