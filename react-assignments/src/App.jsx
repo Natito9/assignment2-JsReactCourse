fb29ce7fd290d67b99d104e7527feb6bf4cac3e1
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-20 10:54:00 +0100

    A9 done

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -3,20 +3,11 @@ import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
 
 function App() {
 
-
-// set state is a function 
-const [submitted,setSubmitted]= useState(false)
-
-const submitForm = () => {
-  setSubmitted(submitted(true), console.log("form submitted"))
-}
-
   return (
     <div>
       <header>
       </header>
       <MyStateComponent />
-      <button onClick={submitForm}>Submit</button>
     </div>
   );
 }
--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -2,16 +2,41 @@ import React, { useState } from "react";
 
 const MyStateComponent = () => {
   // TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
+
+  const [formData, setSubmitData] = useState({ name: "", age: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSubmitData({ ...formData, [name]: value });
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("form submitted:", formData);
+  };
+
   return (
     <div>
       <h1>Form</h1>
-      <form>
-        <label>Name:
-          <input type="text"></input>
+      <form onSubmit={submitForm}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          ></input>
         </label>
-        <label>Age:
-          <input type="text"></input>
+        <label>
+          Age:
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleInputChange}
+          ></input>
         </label>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
